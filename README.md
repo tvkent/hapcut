fbec544bea9e47c1068cde408e3db92f020de116
tvkent <tvkent@ucdavis.edu>
2016-02-04 19:26:59 -0500

    plots

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ Use hapcut reads from _C. grandiflora_ aligned to _C. rubella_ to see if long st
 
 ### Scripts
 * `min_sum.py` takes the hapcut output, parses, sums alt and ref alleles for each block, and tab-deliminates output
+* `hap_plot.R` has the code to plot the span of a block against the sum of the minor? allele binary values in that block
 
 ### Results
 * 3 FEB 2016: ran `min_sum.py` on our 8 test files
 	* 27, 28, 87, 100, 101, 102, 107, 196
+* 4 FEB 2016: plotted hapcut results & highlighted blocks containing part of the putative FT locus
+	* plots in `haps.pdf`	
 
 ### To Do:
-* plot SNPs, length, and SNPs/length by the min sum of 1's in hap block for test 8
 * run hapcut on full 196 dataset, parse, and plot
---
+++ Scripts/hap_plots.R
@@ -0,0 +1,52 @@
+library(data.table)
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+library(wesanderson)
+library(emoGG)
+
+setwd('~/hapcut/Results')
+
+grand <- c(wes_palette("GrandBudapest")[3],wes_palette("GrandBudapest")[2])
+
+cg100 <- fread('100_hapcut.parsed.txt') 
+cg101 <- fread('101_hapcut.parsed.txt')
+cg102 <- fread('102_hapcut.parsed.txt')
+cg107 <- fread('107_hapcut.parsed.txt')
+cg196 <- fread('196_hapcut.parsed.txt')
+cg27 <- fread('27_hapcut.parsed.txt')
+cg28 <- fread('28_hapcut.parsed.txt')
+cg87 <- fread('87_hapcut.parsed.txt')
+
+cg100.FT <- cg100 %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+cg101.FT <- cg101 %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+cg102.FT <- cg102 %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+cg107.FT <- cg107 %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+cg196.FT <- cg196 %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+cg27.FT <- cg27 %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+cg28.FT <- cg28 %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+cg87.FT <- cg87 %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+all.FT <- all %>% filter(chr=='scaffold_2', last>7539046, first<7541032)
+
+all <- rbind(cg100, cg101, cg102, cg107, cg196, cg27, cg28, cg87)
+
+pdf('haps.pdf',width=8, height=5)
+cg100 %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = '100') + geom_point(data = cg100.FT, inherit.aes=TRUE, color=grand[2])
+cg101 %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = '101') + geom_point(data = cg101.FT, inherit.aes=TRUE, color=grand[2])
+cg102 %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = '102') + geom_point(data = cg102.FT, inherit.aes=TRUE, color=grand[2])
+cg107 %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = '107') + geom_point(data = cg107.FT, inherit.aes=TRUE, color=grand[2])
+cg196 %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = '196') + geom_point(data = cg196.FT, inherit.aes=TRUE, color=grand[2])
+cg27 %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = '27') + geom_point(data = cg27.FT, inherit.aes=TRUE, color=grand[2])
+cg28 %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = '28') + geom_point(data = cg28.FT, inherit.aes=TRUE, color=grand[2])
+cg87 %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = '87') + geom_point(data = cg87.FT, inherit.aes=TRUE, color=grand[2])
+all %>% ggplot(aes(x=span, y=minsum)) + geom_point() + geom_smooth(method='lm', se = TRUE, color = grand[1]) +
+  ggtitle(label = 'All') + geom_point(data = all.FT, inherit.aes=TRUE, color=grand[2])
+dev.off()
